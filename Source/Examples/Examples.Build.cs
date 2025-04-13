440efbe8f818f4a9fbd9c4ae4f63af9d33e99a20
weiyiherrington <weiyiweishao@163.com>
2025-04-13 16:04:31 +0800

    remove RuntimeAudioImporter plugin

--- Source/Examples/Examples.Build.cs
+++ Source/Examples/Examples.Build.cs
@@ -8,7 +8,7 @@ public class Examples : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AudioMixer", "RuntimeAudioImporter" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AudioMixer" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] { "AudioExtensions" });
 
